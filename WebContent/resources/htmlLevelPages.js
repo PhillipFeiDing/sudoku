32de355c8f0a46cbf08832d058d40b4c930596d2
Phillip <phillip@lawn-128-61-60-220.lawn.gatech.edu>
2020-01-22 04:05:47 -0500

    updated sudoku game page

---
+++ WebContent/resources/htmlLevelPages.js
@@ -0,0 +1,255 @@
+var page1 = `
+    <div style="position: relative; left: 0px;">
+    <span class= "levelSelect" id="level1">&nbsp; A01 &nbsp;</span>
+    <span id="level2" class= "levelSelect">&nbsp; A02 &nbsp;</span>
+    <span id="level3" class= "levelSelect">&nbsp; A03 &nbsp;</span>
+    <span id="level4" class= "levelSelect">&nbsp; A04 &nbsp;</span>
+    </div>
+    <div style="position: relative; left: 0px;">
+    <span class= "levelSelect" id="level5">&nbsp; A05 &nbsp;</span>
+    <span id="level6" class= "levelSelect">&nbsp; A06 &nbsp;</span>
+    <span id="level7" class= "levelSelect">&nbsp; A07 &nbsp;</span>
+    <span id="level8" class= "levelSelect">&nbsp; A08 &nbsp;</span>
+    </div>
+    <div style="position: relative; left: 0px;">
+    <span class= "levelSelect" id="level9">&nbsp; A09 &nbsp;</span>
+    <span id="level10" class= "levelSelect">&nbsp; A10 &nbsp;</span>
+    <span id="level11" class= "levelSelect">&nbsp; A11 &nbsp;</span>
+    <span id="level12" class= "levelSelect">&nbsp; A12 &nbsp;</span>
+    </div>
+    <div style="position: relative; left: 0px;">
+    <span class= "levelSelect" id="level13">&nbsp; A13 &nbsp;</span>
+    <span id="level14" class= "levelSelect">&nbsp; A14 &nbsp;</span>
+    <span id="level15" class= "levelSelect">&nbsp; A15 &nbsp;</span>
+    <span id="level16" class= "levelSelect">&nbsp; A16 &nbsp;</span>
+    </div>
+    <div style="position: relative; left: 0px;">
+    <span class= "levelSelect" id="level17">&nbsp; A17 &nbsp;</span>
+    <span id="level18" class= "levelSelect">&nbsp; A18 &nbsp;</span>
+    <span id="level19" class= "levelSelect">&nbsp; A19 &nbsp;</span>
+    <span id="level20" class= "levelSelect">&nbsp; A20 &nbsp;</span>
+    </div>
+    <div style="position: relative; left: 0px;">
+    <span class= "levelSelect" id="level21">&nbsp; A21 &nbsp;</span>
+    <span id="level22" class= "levelSelect">&nbsp; A22 &nbsp;</span>
+    <span id="level23" class= "levelSelect">&nbsp; A23 &nbsp;</span>
+    <span id="level24" class= "levelSelect">&nbsp; A24 &nbsp;</span>
+    </div>
+`
+var page2 = `
+    <div style="position: relative; left: 0px;">
+    <span class= "levelSelect" id="level25">&nbsp; A25 &nbsp;</span>
+    <span id="level26" class= "levelSelect">&nbsp; A26 &nbsp;</span>
+    <span id="level27" class= "levelSelect">&nbsp; A27 &nbsp;</span>
+    <span id="level28" class= "levelSelect">&nbsp; A28 &nbsp;</span>
+    </div>
+    <div style="position: relative; left: 0px;">
+    <span class= "levelSelect" id="level29">&nbsp; A29 &nbsp;</span>
+    <span id="level30" class= "levelSelect">&nbsp; A30 &nbsp;</span>
+    <span id="level31" class= "levelSelect">&nbsp; A31 &nbsp;</span>
+    <span id="level32" class= "levelSelect">&nbsp; A32 &nbsp;</span>
+    </div>
+    <div style="position: relative; left: 0px;">
+    <span class= "levelSelect" id="level33">&nbsp; A33 &nbsp;</span>
+    <span id="level34" class= "levelSelect">&nbsp; A34 &nbsp;</span>
+    <span id="level35" class= "levelSelect">&nbsp; A35 &nbsp;</span>
+    <span id="level36" class= "levelSelect">&nbsp; A36 &nbsp;</span>
+    </div>
+    <div style="position: relative; left: 0px;">
+    <span class= "levelSelect" id="level37">&nbsp; A37 &nbsp;</span>
+    <span id="level38" class= "levelSelect">&nbsp; A38 &nbsp;</span>
+    <span id="level39" class= "levelSelect">&nbsp; A39 &nbsp;</span>
+    <span id="level40" class= "levelSelect">&nbsp; A40 &nbsp;</span>
+    </div>
+    <div style="position: relative; left: 0px;">
+    <span class= "levelSelect" id="level41">&nbsp; A41 &nbsp;</span>
+    <span id="level42" class= "levelSelect">&nbsp; A42 &nbsp;</span>
+    <span id="level43" class= "levelSelect">&nbsp; A43 &nbsp;</span>
+    <span id="level44" class= "levelSelect">&nbsp; A44 &nbsp;</span>
+    </div>
+    <div style="position: relative; left: 0px;">
+    <span class= "levelSelect" id="level45">&nbsp; A45 &nbsp;</span>
+    <span id="level46" class= "levelSelect">&nbsp; A46 &nbsp;</span>
+    <span id="level47" class= "levelSelect">&nbsp; A47 &nbsp;</span>
+    <span id="level48" class= "levelSelect">&nbsp; A48 &nbsp;</span>
+    </div>
+`
+
+var page3 = `
+    <div style="position: relative; left: -3px;">
+    <span class= "levelSelect" id="level49">&nbsp; A49 &nbsp;</span>
+    <span id="level50" class= "levelSelect">&nbsp; A50 &nbsp;</span>
+    <span id="level51" class= "levelSelect">&nbsp; B01 &nbsp;</span>
+    <span id="level52" class= "levelSelect">&nbsp; B02 &nbsp;</span>
+    </div>
+    <div style="position: relative; left: -3px;">
+    <span class= "levelSelect" id="level53">&nbsp; B03 &nbsp;</span>
+    <span id="level54" class= "levelSelect">&nbsp; B04 &nbsp;</span>
+    <span id="level55" class= "levelSelect">&nbsp; B05 &nbsp;</span>
+    <span id="level56" class= "levelSelect">&nbsp; B06 &nbsp;</span>
+    </div>
+    <div style="position: relative; left: -3px;">
+    <span class= "levelSelect" id="level57">&nbsp; B07 &nbsp;</span>
+    <span id="level58" class= "levelSelect">&nbsp; B08 &nbsp;</span>
+    <span id="level59" class= "levelSelect">&nbsp; B09 &nbsp;</span>
+    <span id="level60" class= "levelSelect">&nbsp; B10 &nbsp;</span>
+    </div>
+    <div style="position: relative; left: -3px;">
+    <span class= "levelSelect" id="level61">&nbsp; B11 &nbsp;</span>
+    <span id="level62" class= "levelSelect">&nbsp; B12 &nbsp;</span>
+    <span id="level63" class= "levelSelect">&nbsp; B13 &nbsp;</span>
+    <span id="level64" class= "levelSelect">&nbsp; B14 &nbsp;</span>
+    </div>
+    <div style="position: relative; left: -3px;">
+    <span class= "levelSelect" id="level65">&nbsp; B15 &nbsp;</span>
+    <span id="level66" class= "levelSelect">&nbsp; B16 &nbsp;</span>
+    <span id="level67" class= "levelSelect">&nbsp; B17 &nbsp;</span>
+    <span id="level68" class= "levelSelect">&nbsp; B18 &nbsp;</span>
+    </div>
+    <div style="position: relative; left: -3px;">
+    <span class= "levelSelect" id="level69">&nbsp; B19 &nbsp;</span>
+    <span id="level70" class= "levelSelect">&nbsp; B20 &nbsp;</span>
+    <span id="level71" class= "levelSelect">&nbsp; B21 &nbsp;</span>
+    <span id="level72" class= "levelSelect">&nbsp; B22 &nbsp;</span>
+    </div>
+`
+
+var page4 = `
+    <div style="position: relative; left: 0px;">
+    <span class= "levelSelect" id="level73">&nbsp; B23 &nbsp;</span>
+    <span id="level74" class= "levelSelect">&nbsp; B24 &nbsp;</span>
+    <span id="level75" class= "levelSelect">&nbsp; B25 &nbsp;</span>
+    <span id="level76" class= "levelSelect">&nbsp; B26 &nbsp;</span>
+    </div>
+    <div style="position: relative; left: 0px;">
+    <span class= "levelSelect" id="level77">&nbsp; B27 &nbsp;</span>
+    <span id="level78" class= "levelSelect">&nbsp; B28 &nbsp;</span>
+    <span id="level79" class= "levelSelect">&nbsp; B29 &nbsp;</span>
+    <span id="level80" class= "levelSelect">&nbsp; B30 &nbsp;</span>
+    </div>
+    <div style="position: relative; left: 0px;">
+    <span class= "levelSelect" id="level81">&nbsp; B31 &nbsp;</span>
+    <span id="level82" class= "levelSelect">&nbsp; B32 &nbsp;</span>
+    <span id="level83" class= "levelSelect">&nbsp; B33 &nbsp;</span>
+    <span id="level84" class= "levelSelect">&nbsp; B34 &nbsp;</span>
+    </div>
+    <div style="position: relative; left: 0px;">
+    <span class= "levelSelect" id="level85">&nbsp; B35 &nbsp;</span>
+    <span id="level86" class= "levelSelect">&nbsp; B36 &nbsp;</span>
+    <span id="level87" class= "levelSelect">&nbsp; B37 &nbsp;</span>
+    <span id="level88" class= "levelSelect">&nbsp; B38 &nbsp;</span>
+    </div>
+    <div style="position: relative; left: 0px;">
+    <span class= "levelSelect" id="level89">&nbsp; B39 &nbsp;</span>
+    <span id="level90" class= "levelSelect">&nbsp; B40 &nbsp;</span>
+    <span id="level91" class= "levelSelect">&nbsp; B41 &nbsp;</span>
+    <span id="level92" class= "levelSelect">&nbsp; B42 &nbsp;</span>
+    </div>
+    <div style="position: relative; left: 0px;">
+    <span class= "levelSelect" id="level93">&nbsp; B43 &nbsp;</span>
+    <span id="level94" class= "levelSelect">&nbsp; B44 &nbsp;</span>
+    <span id="level95" class= "levelSelect">&nbsp; B45 &nbsp;</span>
+    <span id="level96" class= "levelSelect">&nbsp; B46 &nbsp;</span>
+    </div>
+`
+
+var page5 = `
+    <div style="position: relative; left: -3px;">
+    <span class= "levelSelect" id="level97">&nbsp; B47 &nbsp;</span>
+    <span id="level98" class= "levelSelect">&nbsp; B48 &nbsp;</span>
+    <span id="level99" class= "levelSelect">&nbsp; B49 &nbsp;</span>
+    <span id="level100" class= "levelSelect">&nbsp; B50 &nbsp;</span>
+    </div>
+    <div style="position: relative; left: -3px;">
+    <span class= "levelSelect" id="level101">&nbsp; B51 &nbsp;</span>
+    <span id="level102" class= "levelSelect">&nbsp; B52 &nbsp;</span>
+    <span id="level103" class= "levelSelect">&nbsp; B53 &nbsp;</span>
+    <span id="level104" class= "levelSelect">&nbsp; B54 &nbsp;</span>
+    </div>
+    <div style="position: relative; left: -3px;">
+    <span class= "levelSelect" id="level105">&nbsp; B55 &nbsp;</span>
+    <span id="level106" class= "levelSelect">&nbsp; B56 &nbsp;</span>
+    <span id="level107" class= "levelSelect">&nbsp; B57 &nbsp;</span>
+    <span id="level108" class= "levelSelect">&nbsp; B58 &nbsp;</span>
+    </div>
+    <div style="position: relative; left: -3px;">
+    <span class= "levelSelect" id="level109">&nbsp; B59 &nbsp;</span>
+    <span id="level110" class= "levelSelect">&nbsp; B60 &nbsp;</span>
+    <span id="level111" class= "levelSelect">&nbsp; B61 &nbsp;</span>
+    <span id="level112" class= "levelSelect">&nbsp; B62 &nbsp;</span>
+    </div>
+    <div style="position: relative; left: -3px;">
+    <span class= "levelSelect" id="level113">&nbsp; B63 &nbsp;</span>
+    <span id="level114" class= "levelSelect">&nbsp; B64 &nbsp;</span>
+    <span id="level115" class= "levelSelect">&nbsp; B65 &nbsp;</span>
+    <span id="level116" class= "levelSelect">&nbsp; B66 &nbsp;</span>
+    </div>
+    <div style="position: relative; left: -3px;">
+    <span class= "levelSelect" id="level117">&nbsp; B67 &nbsp;</span>
+    <span id="level118" class= "levelSelect">&nbsp; B68 &nbsp;</span>
+    <span id="level119" class= "levelSelect">&nbsp; B69 &nbsp;</span>
+    <span id="level120" class= "levelSelect">&nbsp; B70 &nbsp;</span>
+    </div>
+`
+
+var page6 = `
+    <div style="position: relative; left: -3px;">
+    <span class= "levelSelect" id="level121">&nbsp; B71 &nbsp;</span>
+    <span id="level122" class= "levelSelect">&nbsp; B72 &nbsp;</span>
+    <span id="level123" class= "levelSelect">&nbsp; B73 &nbsp;</span>
+    <span id="level124" class= "levelSelect">&nbsp; B74 &nbsp;</span>
+    </div>
+    <div style="position: relative; left: -3px;">
+    <span class= "levelSelect" id="level125">&nbsp; B75 &nbsp;</span>
+    <span id="level126" class= "levelSelect">&nbsp; B76 &nbsp;</span>
+    <span id="level127" class= "levelSelect">&nbsp; B77 &nbsp;</span>
+    <span id="level128" class= "levelSelect">&nbsp; B78 &nbsp;</span>
+    </div>
+    <div style="position: relative; left: -3px;">
+    <span class= "levelSelect" id="level129">&nbsp; B79 &nbsp;</span>
+    <span id="level130" class= "levelSelect">&nbsp; B80 &nbsp;</span>
+    <span id="level131" class= "levelSelect">&nbsp; B81 &nbsp;</span>
+    <span id="level132" class= "levelSelect">&nbsp; B82 &nbsp;</span>
+    </div>
+    <div style="position: relative; left: -3px;">
+    <span class= "levelSelect" id="level133">&nbsp; B83 &nbsp;</span>
+    <span id="level134" class= "levelSelect">&nbsp; B84 &nbsp;</span>
+    <span id="level135" class= "levelSelect">&nbsp; B85 &nbsp;</span>
+    <span id="level136" class= "levelSelect">&nbsp; B86 &nbsp;</span>
+    </div>
+    <div style="position: relative; left: -3px;">
+    <span class= "levelSelect" id="level137">&nbsp; B87 &nbsp;</span>
+    <span id="level138" class= "levelSelect">&nbsp; B88 &nbsp;</span>
+    <span id="level139" class= "levelSelect">&nbsp; B89 &nbsp;</span>
+    <span id="level140" class= "levelSelect">&nbsp; B90 &nbsp;</span>
+    </div>
+    <div style="position: relative; left: -3px;">
+    <span class= "levelSelect" id="level141">&nbsp; B91 &nbsp;</span>
+    <span id="level142" class= "levelSelect">&nbsp; B92 &nbsp;</span>
+    <span id="level143" class= "levelSelect">&nbsp; B93 &nbsp;</span>
+    <span id="level144" class= "levelSelect">&nbsp; B94 &nbsp;</span>
+    </div>
+`
+
+var page7 = `
+    <div style="position: relative; left: -5px;">
+    <span class= "levelSelect" id="level145">&nbsp; B95 &nbsp;</span>
+    <span id="level146" class= "levelSelect">&nbsp; C01 &nbsp;</span>
+    <span id="level147" class= "levelSelect">&nbsp; C02 &nbsp;</span>
+    <span id="level148" class= "levelSelect">&nbsp; C03 &nbsp;</span>
+    </div>
+    <div style="position: relative; left:-5px;">
+    <span class= "levelSelect" id="level149">&nbsp; C04 &nbsp;</span>
+    <span id="level150" class= "levelSelect">&nbsp; C05 &nbsp;</span>
+    <span id="level151" class= "levelSelect">&nbsp; C06 &nbsp;</span>
+    <span id="level152" class= "levelSelect">&nbsp; C07 &nbsp;</span>
+    </div>
+    <div style="position: relative; left: -5px;">
+    <span class= "levelSelect" id="level153">&nbsp; C08 &nbsp;</span>
+    <span id="level154" class= "levelSelect">&nbsp; C09 &nbsp;</span>
+    <span id="level155" class= "levelSelect">&nbsp; C10 &nbsp;</span>
+    <span id="level156" class= "levelSelect">&nbsp; C11 &nbsp;</span>
+    </div>
+`
+
+var pages = [page1, page2, page3, page4, page5, page6, page7]
---
+++ WebContent/resources/htmlTable.js
@@ -0,0 +1,188 @@
+var htmlTable = 
+`            
+    <div class="table">
+        <div class="subTable">
+            <div class="row">
+                <div class="entry blank" row="0" col="0"></div>
+                <div class="entry blank" row="0" col="1"></div>
+                <div class="entry blank" row="0" col="2"></div>
+                <div class="clearFloat"></div>
+            </div>
+            <div class="row">
+                <div class="entry blank" row="1" col="0"></div>
+                <div class="entry blank" row="1" col="1"></div>
+                <div class="entry blank" row="1" col="2"></div>
+                <div class="clearFloat"></div>
+            </div>
+            <div class="row">
+                <div class="entry blank" row="2" col="0"></div>
+                <div class="entry blank" row="2" col="1"></div>
+                <div class="entry blank" row="2" col="2"></div>
+                <div class="clearFloat"></div>
+            </div>
+        </div>
+        <div class="subTable">
+            <div class="row">
+                <div class="entry blank" row="0" col="3"></div>
+                <div class="entry blank" row="0" col="4"></div>
+                <div class="entry blank" row="0" col="5"></div>
+                <div class="clearFloat"></div>
+            </div>
+            <div class="row">
+                <div class="entry blank" row="1" col="3"></div>
+                <div class="entry blank" row="1" col="4"></div>
+                <div class="entry blank" row="1" col="5"></div>
+                <div class="clearFloat"></div>
+            </div>
+            <div class="row">
+                <div class="entry blank" row="2" col="3"></div>
+                <div class="entry blank" row="2" col="4"></div>
+                <div class="entry blank" row="2" col="5"></div>
+                <div class="clearFloat"></div>
+            </div>
+        </div>
+        <div class="subTable">
+            <div class="row">
+                <div class="entry blank" row="0" col="6"></div>
+                <div class="entry blank" row="0" col="7"></div>
+                <div class="entry blank" row="0" col="8"></div>
+                <div class="clearFloat"></div>
+            </div>
+            <div class="row">
+                <div class="entry blank" row="1" col="6"></div>
+                <div class="entry blank" row="1" col="7"></div>
+                <div class="entry blank" row="1" col="8"></div>
+                <div class="clearFloat"></div>
+            </div>
+            <div class="row">
+                <div class="entry blank" row="2" col="6"></div>
+                <div class="entry blank" row="2" col="7"></div>
+                <div class="entry blank" row="2" col="8"></div>
+                <div class="clearFloat"></div>
+            </div>
+        </div>
+        <div class="clearFloat"></div>
+        <div class="subTable">
+            <div class="row">
+                <div class="entry blank" row="3" col="0"></div>
+                <div class="entry blank" row="3" col="1"></div>
+                <div class="entry blank" row="3" col="2"></div>
+                <div class="clearFloat"></div>
+            </div>
+            <div class="row">
+                <div class="entry blank" row="4" col="0"></div>
+                <div class="entry blank" row="4" col="1"></div>
+                <div class="entry blank" row="4" col="2"></div>
+                <div class="clearFloat"></div>
+            </div>
+            <div class="row">
+                <div class="entry blank" row="5" col="0"></div>
+                <div class="entry blank" row="5" col="1"></div>
+                <div class="entry blank" row="5" col="2"></div>
+                <div class="clearFloat"></div>
+            </div>
+        </div>
+        <div class="subTable">
+            <div class="row">
+                <div class="entry blank" row="3" col="3"></div>
+                <div class="entry blank" row="3" col="4"></div>
+                <div class="entry blank" row="3" col="5"></div>
+                <div class="clearFloat"></div>
+            </div>
+            <div class="row">
+                <div class="entry blank" row="4" col="3"></div>
+                <div class="entry blank" row="4" col="4"></div>
+                <div class="entry blank" row="4" col="5"></div>
+                <div class="clearFloat"></div>
+            </div>
+            <div class="row">
+                <div class="entry blank" row="5" col="3"></div>
+                <div class="entry blank" row="5" col="4"></div>
+                <div class="entry blank" row="5" col="5"></div>
+                <div class="clearFloat"></div>
+            </div>
+        </div>
+        <div class="subTable">
+            <div class="row">
+                <div class="entry blank" row="3" col="6"></div>
+                <div class="entry blank" row="3" col="7"></div>
+                <div class="entry blank" row="3" col="8"></div>
+                <div class="clearFloat"></div>
+            </div>
+            <div class="row">
+                <div class="entry blank" row="4" col="6"></div>
+                <div class="entry blank" row="4" col="7"></div>
+                <div class="entry blank" row="4" col="8"></div>
+                <div class="clearFloat"></div>
+            </div>
+            <div class="row">
+                <div class="entry blank" row="5" col="6"></div>
+                <div class="entry blank" row="5" col="7"></div>
+                <div class="entry blank" row="5" col="8"></div>
+                <div class="clearFloat"></div>
+            </div>
+        </div>
+        <div class="clearFloat"></div>
+        <div class="subTable">
+            <div class="row">
+                <div class="entry blank" row="6" col="0"></div>
+                <div class="entry blank" row="6" col="1"></div>
+                <div class="entry blank" row="6" col="2"></div>
+                <div class="clearFloat"></div>
+            </div>
+            <div class="row">
+                <div class="entry blank" row="7" col="0"></div>
+                <div class="entry blank" row="7" col="1"></div>
+                <div class="entry blank" row="7" col="2"></div>
+                <div class="clearFloat"></div>
+            </div>
+            <div class="row">
+                <div class="entry blank" row="8" col="0"></div>
+                <div class="entry blank" row="8" col="1"></div>
+                <div class="entry blank" row="8" col="2"></div>
+                <div class="clearFloat"></div>
+            </div>
+        </div>
+        <div class="subTable">
+            <div class="row">
+                <div class="entry blank" row="6" col="3"></div>
+                <div class="entry blank" row="6" col="4"></div>
+                <div class="entry blank" row="6" col="5"></div>
+                <div class="clearFloat"></div>
+            </div>
+            <div class="row">
+                <div class="entry blank" row="7" col="3"></div>
+                <div class="entry blank" row="7" col="4"></div>
+                <div class="entry blank" row="7" col="5"></div>
+                <div class="clearFloat"></div>
+            </div>
+            <div class="row">
+                <div class="entry blank" row="8" col="3"></div>
+                <div class="entry blank" row="8" col="4"></div>
+                <div class="entry blank" row="8" col="5"></div>
+                <div class="clearFloat"></div>
+            </div>
+        </div>
+        <div class="subTable">
+            <div class="row">
+                <div class="entry blank" row="6" col="6"></div>
+                <div class="entry blank" row="6" col="7"></div>
+                <div class="entry blank" row="6" col="8"></div>
+                <div class="clearFloat"></div>
+            </div>
+            <div class="row">
+                <div class="entry blank" row="7" col="6"></div>
+                <div class="entry blank" row="7" col="7"></div>
+                <div class="entry blank" row="7" col="8"></div>
+                <div class="clearFloat"></div>
+            </div>
+            <div class="row">
+                <div class="entry blank" row="8" col="6"></div>
+                <div class="entry blank" row="8" col="7"></div>
+                <div class="entry blank" row="8" col="8"></div>
+                <div class="clearFloat"></div>
+            </div>
+        </div>
+        <div class="clearFloat"></div>
+    </div>
+`
